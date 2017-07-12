63dc142ed00e7fbad6472c797ec2f91e72ab7559
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2017-07-11 20:08:58 -0400

    Pass data to the plugin.

--- emptest/plugin-dev/plugin-dev.js
+++ emptest/plugin-dev/plugin-dev.js
@@ -1,7 +1,7 @@
-videojs.plugin('pluginDev', function() {
+videojs.plugin('pluginDev', function(options) {
   var player = this,
   overlay = document.createElement('p');
   overlay.className = 'vjs-overlay';
-  overlay.innerHTML = "Becoming a plugin developer";
+  overlay.innerHTML = (options != null && options.overlayText != null) ? options.overlayText : "Becoming a plugin developer";
   player.el().appendChild(overlay);
 });
